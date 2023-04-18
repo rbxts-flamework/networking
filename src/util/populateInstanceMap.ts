163c3789a71eff89451b3c0cdd69f83798c3ffee
Jack Taylor <44332148+jackTabsCode@users.noreply.github.com>
2023-04-18 11:42:58 -0700

    create empty remotes

--- src/util/populateInstanceMap.ts
+++ src/util/populateInstanceMap.ts
@@ -6,6 +6,15 @@ export function populateInstanceMap<T extends "RemoteEvent" | "RemoteFunction">(
 	names: string[],
 	map: Map<string, CreatableInstances[T]>,
 ) {
+	if (!RunService.IsRunning()) {
+		for (const name of names) {
+			const instance = new Instance(className);
+			instance.Name = name;
+			map.set(name, instance);
+		}
+		return;
+	}
+	
 	let remotes = RunService.IsServer()
 		? ReplicatedStorage.FindFirstChild(globalName)
 		: ReplicatedStorage.WaitForChild(globalName);
