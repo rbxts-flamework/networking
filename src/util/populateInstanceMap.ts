db0142f505d4162087c908bc79e68a928bef8761
fireboltofdeath <24422634+Fireboltofdeath@users.noreply.github.com>
2023-04-21 08:17:02 -0400

    Switch back to IsRunning

--- src/util/populateInstanceMap.ts
+++ src/util/populateInstanceMap.ts
@@ -6,7 +6,7 @@ export function populateInstanceMap<T extends "RemoteEvent" | "RemoteFunction">(
 	names: string[],
 	map: Map<string, CreatableInstances[T]>,
 ) {
-	if (RunService.IsEdit()) {
+	if (!RunService.IsRunning()) {
 		for (const name of names) {
 			const instance = new Instance(className);
 			instance.Name = name;
@@ -14,7 +14,7 @@ export function populateInstanceMap<T extends "RemoteEvent" | "RemoteFunction">(
 		}
 		return;
 	}
-	
+
 	let remotes = RunService.IsServer()
 		? ReplicatedStorage.FindFirstChild(globalName)
 		: ReplicatedStorage.WaitForChild(globalName);
